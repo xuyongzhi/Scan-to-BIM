35dbb935037578950ed2efd0df4cd3b63757ae7c
xuyongzhi <buaaxyz@yeah.net>
2020-01-10 21:57:05 +1100

    add sh

---
+++ test.sh
@@ -0,0 +1,2 @@
+ipython tools/test.py -- configs/reppoints/reppoints_moment_r50_fpn_2x.py  checkpoints/reppoints_moment_r101_dcn_fpn_2x.pth --out res/res.pickle
+
---
+++ train.sh
@@ -0,0 +1,2 @@
+ipython tools/train.py -- configs/reppoints/reppoints_moment_r50_fpn_1x.py
+
