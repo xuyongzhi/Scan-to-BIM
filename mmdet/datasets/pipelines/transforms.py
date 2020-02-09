a127214703e8d668936582931e0ea20dc198ee0a
xuyongzhi <buaaxyz@yeah.net>
2020-02-09 22:44:06 +1100

    new config

--- mmdet/datasets/pipelines/transforms.py
+++ mmdet/datasets/pipelines/transforms.py
@@ -999,3 +999,115 @@ class Albu(object):
         repr_str = self.__class__.__name__
         repr_str += '(transformations={})'.format(self.transformations)
         return repr_str
+
+
+@PIPELINES.register_module
+class RandomRotate(object):
+    """Flip the image & bbox & mask.
+
+    If the input dict contains the key "flip", then the flag will be used,
+    otherwise it will be randomly decided by a ratio specified in the init
+    method.
+
+    Args:
+        flip_ratio (float, optional): The flipping probability.
+    """
+
+    def __init__(self, flip_ratio=None, direction='horizontal', obj_rep='box_scope'):
+        self.flip_ratio = flip_ratio
+        self.direction = direction
+        self.obj_rep = obj_rep
+        if flip_ratio is not None:
+            assert flip_ratio >= 0 and flip_ratio <= 1
+        assert direction in ['horizontal', 'vertical', 'random']
+        assert obj_rep in ['box_scope', 'line_scope', 'lscope_istopleft']
+
+    def bbox_flip(self, bboxes, img_shape, direction, obj_rep):
+        """Flip bboxes horizontally.
+
+        Args:
+            bboxes(ndarray): shape (..., 4*k)
+            img_shape(tuple): (height, width)
+        """
+        if obj_rep == 'box_scope' or obj_rep == 'line_scope':
+          return self.bbox_flip_scope(bboxes, img_shape, direction)
+        elif obj_rep == 'lscope_istopleft':
+          return self.bbox_flip_scope_itl(bboxes, img_shape, direction)
+        else:
+          raise NotImplementedError
+
+    def bbox_flip_scope_itl(self, bboxes, img_shape, direction):
+        assert bboxes.shape[-1] == 5
+        flipped = bboxes.copy()
+        if direction == 'horizontal':
+            w = img_shape[1]
+            flipped[..., 0] = w - bboxes[..., 2] - 1
+            flipped[..., 2] = w - bboxes[..., 0] - 1
+        elif direction == 'vertical':
+            h = img_shape[0]
+            flipped[..., 1] = h - bboxes[..., 3] - 1
+            flipped[..., 3] = h - bboxes[..., 1] - 1
+        else:
+            raise ValueError(
+                'Invalid flipping direction "{}"'.format(direction))
+        flipped[:,-1] = -flipped[:,-1]
+        #mask = (np.abs(flipped[:,-1]) >2).reshape(-1,1)
+        #flipped = flipped - mask * flipped * 2
+        #print('\nflipped\n')
+        return flipped
+
+    def bbox_flip_scope(self, bboxes, img_shape, direction):
+        assert bboxes.shape[-1] % 4 == 0
+        flipped = bboxes.copy()
+        if direction == 'horizontal':
+            w = img_shape[1]
+            flipped[..., 0::4] = w - bboxes[..., 2::4] - 1
+            flipped[..., 2::4] = w - bboxes[..., 0::4] - 1
+        elif direction == 'vertical':
+            h = img_shape[0]
+            flipped[..., 1::4] = h - bboxes[..., 3::4] - 1
+            flipped[..., 3::4] = h - bboxes[..., 1::4] - 1
+        else:
+            raise ValueError(
+                'Invalid flipping direction "{}"'.format(direction))
+        return flipped
+
+    def __call__(self, results):
+        if 'flip' not in results:
+            flip = True if np.random.rand() < self.flip_ratio else False
+            results['flip'] = flip
+        if 'flip_direction' not in results:
+            results['flip_direction'] = self.direction
+        if results['flip_direction'] == 'random':
+          i = np.random.choice([0,1])
+          results['flip_direction'] = ['horizontal', 'vertical'][i]
+          #print(results['flip_direction'])
+        if 'obj_rep' not in results:
+            results['obj_rep'] = self.obj_rep
+        if results['flip']:
+            # flip image
+            results['img'] = mmcv.imflip(
+                results['img'], direction=results['flip_direction'])
+            # flip bboxes
+            for key in results.get('bbox_fields', []):
+                results[key] = self.bbox_flip(results[key],
+                                              results['img_shape'],
+                                              results['flip_direction'],
+                                              results['obj_rep'])
+            # flip masks
+            for key in results.get('mask_fields', []):
+                results[key] = [
+                    mmcv.imflip(mask, direction=results['flip_direction'])
+                    for mask in results[key]
+                ]
+
+            # flip segs
+            for key in results.get('seg_fields', []):
+                results[key] = mmcv.imflip(
+                    results[key], direction=results['flip_direction'])
+        return results
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(flip_ratio={})'.format(
+            self.flip_ratio)
+
