98effe67bb90f00e72cd9329705af4b9b0114d7c
xuyongzhi <buaaxyz@yeah.net>
2020-02-12 00:14:41 +1100

    new config

--- train_bk.sh
+++ train_bk.sh
@@ -1,14 +1,12 @@
-export CUDA_VISIBLE_DEVICES=1
+#export CUDA_VISIBLE_DEVICES=1
 CONFIG=configs/strpoints/strpoints_r50_fpn_1x.py
-#CONFIG=configs/strpoints/strpoints_r50_fpn_1x_lr02.py
 
-ipython tools/train.py -- ${CONFIG} 
+#ipython tools/train.py -- ${CONFIG} 
 
+./tools/dist_train.sh ${CONFIG} 2
+
+CONFIG=configs/strpoints/strpoints_r50_fpn_1x_B.py
 #./tools/dist_train.sh ${CONFIG} 2
 
-CONFIG=configs/strpoints/strpoints_r50_fpn_1x_lr5.py
+CONFIG=configs/strpoints/strpoints_r50_fpn_1x_slr.py
 #./tools/dist_train.sh ${CONFIG} 2
-
-CONFIG=configs/strpoints/strpoints_r50_fpn_1x_lr05.py
-#./tools/dist_train.sh ${CONFIG} 2
-
