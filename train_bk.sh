63e1f7aea521abdddc03eb05b9a1eb5caa0be0cf
xuyongzhi <buaaxyz@yeah.net>
2020-02-14 17:35:02 +1100

    ju

--- train_bk.sh
+++ train_bk.sh
@@ -3,4 +3,4 @@ CONFIG=configs/strpoints/strpoints_r50_fpn_1x.py
 
 #ipython tools/train.py --  ${CONFIG}  --rotate 0 --lr 0.05 --bs 3
 
-./tools/dist_train.sh ${CONFIG} 2  --rotate 0 --lr 0.05 --bs 3
+./tools/dist_train.sh ${CONFIG} 2  --rotate 0 --lr 0.01
