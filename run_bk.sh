ab0691f51d8bb095050a59d4599f7abe8592589f
xuyongzhi <buaaxyz@yeah.net>
2020-03-07 23:50:04 +1100

    ju

--- run_bk.sh
+++ run_bk.sh
@@ -15,10 +15,15 @@ CLS=refine_final
 CORHM=1
 DCN_ZERO_BASE=0
 
-ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
+#ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
 
-#./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
+./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
 
+
+CONFIG=configs/strpoints/strpoints_r50_fpn_1x_b4.py
+./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
+CONFIG=configs/strpoints/strpoints_r50_fpn_1x_Fpn6L.py
+./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
 
 
 ROTATE=0
