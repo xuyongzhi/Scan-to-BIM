e9155cf5f7f9c6a3d8c5c952f7f56fe198ce4ae3
xuyongzhi <buaaxyz@yeah.net>
2020-03-07 23:22:16 +1100

    fix a bug: len(self.point_strides )=5 when fpn num level=5

--- run_bk.sh
+++ run_bk.sh
@@ -7,6 +7,7 @@ CONFIG=configs/strpoints/strpoints_r50_fpn_1x.py
 
 #CP=./checkpoints/refine_best_20Feb.pth
 wkdir=T90_r50_fpn_lscope_istopleft_refine_final_512_VerD_bs6_lr10_RA_Normrawstd_ChmR2P1N1_Rfiou743_Fpn45
+wkdir=T90_r50_fpn_lscope_istopleft_refine_final_1024_VerD_bs1_lr5_RA_Normrawstd_ChmR2P1N1_Rfiou743_Fpn45
 CP=./work_dirs/${wkdir}/best.pth
 
 ROTATE=1
@@ -14,7 +15,7 @@ CLS=refine_final
 CORHM=1
 DCN_ZERO_BASE=0
 
-#ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE --resume $CP
+ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
 
 #./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
 
@@ -23,6 +24,6 @@ DCN_ZERO_BASE=0
 ROTATE=0
 STYLE='--out ./work_dirs/'${wkdir}'/detection.pickle --eval bbox'
 
-ipython tools/test.py --  ${CONFIG} $CP --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE $STYLE
+#ipython tools/test.py --  ${CONFIG} $CP --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE $STYLE
 
 
