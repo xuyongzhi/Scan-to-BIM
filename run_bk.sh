fa9a1bf66bb176f79ed1d0f4125d22f472d5b6e3
xuyongzhi <buaaxyz@yeah.net>
2020-03-09 10:43:10 +1100

    tag: before using pcl

--- run_bk.sh
+++ run_bk.sh
@@ -6,7 +6,8 @@
 CONFIG=configs/strpoints/_strpoints_r50_fpn_1x.py
 CONFIG=configs/strpoints/strpoints_r50_fpn_1x_IoU844.py
 
-wkdir=T90_r50_fpn_lscope_istopleft_refine_final_512_VerD_bs6_lr10_RA_Normrawstd_ChmR2P1N1_Rfiou743_Fpn34_Pbs1
+
+wkdir=T90_r50_fpn_lscope_istopleft_refine_final_1024_VerD_bs1_lr50_RA_Normrawstd_ChmR2P1N1_Rfiou743_Fpn34_Pbs1
 CP=./work_dirs/${wkdir}/best.pth
 
 ROTATE=1
@@ -15,15 +16,15 @@ CORHM=1
 DCN_ZERO_BASE=0
 
 
-ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE 
+#ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE 
 
-#./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
+#./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE --lr 0.05
 
 
 ROTATE=0
 STYLE='--out ./work_dirs/'${wkdir}'/detection.pickle --eval bbox'
 #STYLE=--show
 
-#ipython tools/test.py --  ${CONFIG} $CP --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE $STYLE
+ipython tools/test.py --  ${CONFIG} $CP --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE $STYLE
 
 
