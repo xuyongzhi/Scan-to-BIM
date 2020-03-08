328deb4038d11aaffa4aa3ae24bcbadc7c1570bc
xuyongzhi <buaaxyz@yeah.net>
2020-03-09 10:20:17 +1100

    new config

--- run_bk.sh
+++ run_bk.sh
@@ -3,26 +3,20 @@
 #export CUDA_LAUNCH_BLOCKING=1
 #export CUDA_VISIBLE_DEVICES=1
 
-CONFIG=configs/strpoints/strpoints_r50_fpn_1x.py
-CONFIG=configs/strpoints/_strpoints_r50_fpn_1x_Fpn34_bs1.py
+CONFIG=configs/strpoints/_strpoints_r50_fpn_1x.py
+CONFIG=configs/strpoints/strpoints_r50_fpn_1x_IoU844.py
 
 wkdir=T90_r50_fpn_lscope_istopleft_refine_final_512_VerD_bs6_lr10_RA_Normrawstd_ChmR2P1N1_Rfiou743_Fpn34_Pbs1
 CP=./work_dirs/${wkdir}/best.pth
 
-ROTATE=0
+ROTATE=1
 CLS=refine_final
 CORHM=1
 DCN_ZERO_BASE=0
 
 
-#ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE --resume $CP
+ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE 
 
-#./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
-
-
-#CONFIG=configs/strpoints/strpoints_r50_fpn_1x_IoU633.py
-#./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
-#CONFIG=configs/strpoints/strpoints_r50_fpn_1x_IoU740.py
 #./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE
 
 
@@ -30,6 +24,6 @@ ROTATE=0
 STYLE='--out ./work_dirs/'${wkdir}'/detection.pickle --eval bbox'
 #STYLE=--show
 
-ipython tools/test.py --  ${CONFIG} $CP --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE $STYLE
+#ipython tools/test.py --  ${CONFIG} $CP --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE $STYLE
 
 
