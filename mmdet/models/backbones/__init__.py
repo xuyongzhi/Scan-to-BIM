ccac917437519e83eed98fb5be7c3ea63f3d02af
xuyongzhi <buaaxyz@yeah.net>
2020-04-07 17:24:16 +1000

    ju

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -4,7 +4,6 @@ from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 
 from .vox_resnet import VoxResNet, make_vox_res_layer
-from .vox_dense_resnet import VoxDenseResNet
 from .sparse3d_resnet import Sparse3DResNet
 
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'VoxResNet', 'make_vox_res_layer', 'VoxDenseResNet', 'Sparse3DResNet']
+__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'VoxResNet', 'make_vox_res_layer', 'Sparse3DResNet']
