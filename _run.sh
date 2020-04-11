90246adc11743e68abae4c729b4c311cb460a2f6
xuyongzhi <buaaxyz@yeah.net>
2020-04-11 11:14:21 +1000

    orgnize configs

---
+++ _run.sh
@@ -0,0 +1,37 @@
+# xyz
+
+#export CUDA_LAUNCH_BLOCKING=1
+#export CUDA_VISIBLE_DEVICES=1
+
+CONFIG=configs/strpoints/strpoints_r50_fpn_1x.py
+#CONFIG=configs/strpoints/bev_sparse_strpoints_r50_fpn_1x.py
+#CONFIG=configs/strpoints/pcl_strpoints_r50_fpn_1x.py
+CONFIG=configs/strpoints/pcl_dense3dfpn_strpoints_r50_fpn_1x.py
+#CONFIG=configs/strpoints/pcl_dense3dfpn_strpoints_r50_fpn_1x_mc.py
+
+
+wkdir=R50_fpn_refine_final_beike_pcl_2d_bs5_lr10_cnx_Daug_Rfiou743_Fpn34_Pbs1_Bp32_Vsz4Stem4
+CP=./work_dirs/${wkdir}/best.pth
+CONFIG=./work_dirs/${wkdir}/_pcl_dense3dfpn_strpoints_r50_fpn_1x.py
+
+
+ROTATE=1
+CLS=refine_final
+CORHM=0
+DCN_ZERO_BASE=0
+BASE_PLANE=32
+BS=5
+DATA_TYPES=cnx
+
+#ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE  --lr 0.01 --base_plane $BASE_PLANE --bs $BS  --data_types $DATA_TYPES
+#--resume $CP 
+
+
+
+ROTATE=0
+STYLE='--out ./work_dirs/'${wkdir}'/detection.pickle --eval bbox'
+#STYLE=--show
+
+ipython tools/test.py --  ${CONFIG} $CP --rotate $ROTATE --cls $CLS --corhm $CORHM --dcn_zero_base $DCN_ZERO_BASE $STYLE --base_plane $BASE_PLANE  --data_types $DATA_TYPES
+
+
