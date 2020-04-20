ae3ca19d35c6093addd5136bd69dfb8faf7e5d7b
xuyongzhi <buaaxyz@yeah.net>
2020-04-20 22:17:10 +1000

    ju

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #export CUDA_VISIBLE_DEVICES=1
 
 CONFIG=configs/strpoints/strpoints_r50_fpn_1x.py
-CONFIG=configs/strpoints/S3dProj_BevResNet_strpoints_r50_fpn_1x.py
+#CONFIG=configs/strpoints/S3dProj_BevResNet_strpoints_r50_fpn_1x.py
 
 
 wkdir=TPV_r50_fpn_beike2d_wa_bs6_lr10_LsW510_RAR2P1N1_Rfiou743_Fpn44_Pbs1_Bp32-D90_0K_zX
