cc825591b3710d37e4f80ac7c508949c18bff167
xuyongzhi <buaaxyz@yeah.net>
2020-05-07 23:56:25 +1000

    ju

--- configs/strpoints/bev_strpoints_r50_fpn_1x.py
+++ configs/strpoints/bev_strpoints_r50_fpn_1x.py
@@ -263,7 +263,7 @@ if DATA == 'beike2d':
 elif DATA == 'stanford2d':
   load_from = './checkpoints/sfd/Apr26_wabeco_Bev.pth'
   load_from = './checkpoints/sfd/Rect4CornersZ0Z1_1.pth'
-#load_from = None
+load_from = None
 resume_from = None
 auto_resume = True
 workflow = [('train', 5), ('val', 1)]
