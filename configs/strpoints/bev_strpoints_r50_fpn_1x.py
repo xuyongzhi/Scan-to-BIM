1137c459934af43b71464742b0a9a8a14e4a9584
xuyongzhi <buaaxyz@yeah.net>
2020-05-08 19:13:57 +1000

    ju

--- configs/strpoints/bev_strpoints_r50_fpn_1x.py
+++ configs/strpoints/bev_strpoints_r50_fpn_1x.py
@@ -263,13 +263,12 @@ if DATA == 'beike2d':
 elif DATA == 'stanford2d':
   load_from = './checkpoints/sfd/Apr26_wabeco_Bev.pth'
   load_from = './checkpoints/sfd/Rect4CornersZ0Z1_r.pth'
-  #load_from = './checkpoints/sfd/Rect4CornersZ0Z1_nr.pth'
-  load_from = '/home/z/Research/mmdetection/work_dirs/SFD_BP/May7_1File/Area_2_conferenceRoom_1/sTPV_r50_fpn_Rect4CornersZ0Z1_RIou_Nla9_stanford2d_wabecowi_bs1_lr10_LsW510R2P1N1_Rfiou741_Fpn44_Pbs1_Bp32_Fe/best.pth'
-load_from = None
+  load_from = './checkpoints/sfd/Rect4CornersZ0Z1_NR_1.pth'
+#load_from = None
 resume_from = None
 auto_resume = True
 workflow = [('train', 5), ('val', 1)]
-if 1:
+if 0:
   data['workers_per_gpu'] = 0
   workflow = [('train', 1),]
   checkpoint_config = dict(interval=100)
