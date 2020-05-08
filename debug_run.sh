e9f4a36c7ad68ab61ed1d496906f166ef912f3d6
xuyongzhi <buaaxyz@yeah.net>
2020-05-08 19:09:01 +1000

    ju

--- debug_run.sh
+++ debug_run.sh
@@ -29,7 +29,8 @@ CLS=a
 #CLS=c
 CLS=abci
 
-ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --lr $LR --base_plane $BASE_PLANE --bs $BS  --data_types $DATA_TYPES  --filter_edges $FILTER_EDGES  --classes $CLS --relation $REL   --resume $CP 
+ipython tools/train.py --  ${CONFIG} --rotate $ROTATE --lr $LR --base_plane $BASE_PLANE --bs $BS  --data_types $DATA_TYPES  --filter_edges $FILTER_EDGES  --classes $CLS --relation $REL  
+#--resume $CP 
 
 #./tools/dist_train.sh ${CONFIG} 2 --rotate $ROTATE   --lr $LR --base_plane $BASE_PLANE   --data_types $DATA_TYPES  --filter_edges $FILTER_EDGES  --classes $CLS   --relation $REL
 #--resume $CP 
