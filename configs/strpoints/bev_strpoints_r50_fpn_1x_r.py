6978c3f4fa57c7b307071e80f72988ff177e78e3
xuyongzhi <buaaxyz@yeah.net>
2020-06-19 20:26:49 +1000

    ju

--- configs/strpoints/bev_strpoints_r50_fpn_1x_r.py
+++ configs/strpoints/bev_strpoints_r50_fpn_1x_r.py
@@ -234,17 +234,17 @@ data = dict(
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
-total_epochs =  10
+total_epochs =  8
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=20,
     warmup_ratio=1.0 / 3,
-    step=[int(total_epochs*0.4), int(total_epochs*0.7)])
-checkpoint_config = dict(interval=5)
+    step=[int(total_epochs*0.4), int(total_epochs*0.8)])
+checkpoint_config = dict(interval=2)
 # yapf:disable
 log_config = dict(
-    interval=10,
+    interval=5,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook')
