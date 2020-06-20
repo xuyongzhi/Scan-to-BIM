e2c920d51343892bd5ed438fe09066880714b02b
xuyongzhi <buaaxyz@yeah.net>
2020-06-20 10:55:09 +1000

    ju

--- configs/strpoints/bev_strpoints_r50_fpn_1x.py
+++ configs/strpoints/bev_strpoints_r50_fpn_1x.py
@@ -244,7 +244,7 @@ lr_config = dict(
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
-    interval=2,
+    interval=1,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook')
